5fe7b1f1873a9add04f3142e4a4c2e1b87b53fcf
Mark Nichting <mtnichting@gmail.com>
2024-11-20 21:44:52 -0800

    added list virtualization

--- src/app/components/Feed/Feed.tsx
+++ src/app/components/Feed/Feed.tsx
@@ -1,15 +1,52 @@
+import { memo, useRef } from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
 import { MessageType } from '@/app/page';
 import Message from '../Message';
 
 const Feed = ({ messages }: { messages: MessageType[] }) => {
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const virtualizer = useVirtualizer({
+    count: messages.length,
+    estimateSize: () => 100,
+    getScrollElement: () => parentRef.current,
+    overscan: 5,
+    measureElement: (element) => {
+      return element?.getBoundingClientRect().height || 100;
+    },
+  });
+
   return (
-    <div className="feed bg-slate-200 rounded overflow-scroll p-4	w-full">
+    <div className="feed bg-slate-200 rounded  overflow-auto p-4	w-full" ref={parentRef}>
       <h2 className="mb-2">Messages</h2>
-      {messages.map((message) => {
-        return <Message key={message.id} {...message} />;
-      })}
+      <div
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+          width: '100%',
+          position: 'relative',
+        }}
+      >
+        {virtualizer.getVirtualItems().map((virtualRow) => {
+          const message = messages[virtualRow.index];
+          return (
+            <Message
+              key={message.id}
+              virtualIndex={virtualRow.index}
+              {...message}
+              refProp={virtualizer.measureElement}
+              virtualStyles={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                transform: `translateY(${virtualRow.start}px)`,
+              }}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Feed;
+export default memo(Feed);
--- src/app/components/Message/Message.tsx
+++ src/app/components/Message/Message.tsx
@@ -1,16 +1,42 @@
 import Image from 'next/image';
 import { MessageType } from '@/app/page';
 
-const Message = ({ username, message, timestamp, profilePicture }: MessageType) => {
+type VirtualStyles = {
+  position: 'absolute';
+  top: number;
+  left: number;
+  width: string;
+  transform: string;
+};
+type MessageProps = MessageType & {
+  virtualStyles: VirtualStyles;
+  refProp: (node: Element | null | undefined) => void;
+  virtualIndex: number;
+};
+
+const Message = ({
+  username,
+  message,
+  timestamp,
+  profilePicture,
+  refProp,
+  virtualStyles,
+  virtualIndex,
+}: MessageProps) => {
   return (
-    <div className="flex items-center gap-3 p-4 hover:bg-gray-50">
+    <div
+      className="flex items-center gap-3 p-4 hover:bg-gray-50"
+      style={virtualStyles}
+      ref={(el) => refProp(el)}
+      data-index={virtualIndex}
+    >
       <Image
         src={profilePicture}
         alt={`${username}'s profile`}
+        unoptimized
         width={40}
         height={40}
         className="w-10 h-10 rounded-full object-cover"
-        unoptimized
       />
       <div className="flex-1">
         <div className="flex items-center gap-2">
